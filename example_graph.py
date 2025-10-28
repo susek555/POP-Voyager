0a92d7c500cba41c3fa3958624ac50988f669c43
Michal <01187288@pw.edu.pl>
2025-10-28 13:43:51 +0100

    Added function to generate graph

--- example_graph.py
+++ example_graph.py
@@ -1,15 +1,13 @@
 import networkx as nx
 import draw_graph
+import generate_graph
 
 # Tworzymy graf
-G = nx.Graph()
-
-G.add_node("P", reward=0, pos=(0, 0, 0))
-G.add_node("s1", reward=15, pos=(1, 2, 0.5))
-G.add_node("s2", reward=20, pos=(2, 0, 1))
-
-G.add_edge("P", "s1", cost=5)
-G.add_edge("P", "s2", cost=10)
-G.add_edge("s1", "s2", cost=3)
+G = generate_graph.generate_graph(
+    number_of_nodes=10,
+    # max_base_distance=10.0,
+    # reward_range=(5, 20),
+    # cost_factor=1.0
+)
 
 draw_graph.draw_graph(G, "Example 3D Graph")
