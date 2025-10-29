1f8d34fe7929474971985545d196dfd0afff6175
Michal <01187288@pw.edu.pl>
2025-10-29 19:25:08 +0100

    added target function

--- example_graph.py
+++ example_graph.py
@@ -1,13 +1,18 @@
 import networkx as nx
 import draw_graph
 import generate_graph
+from target_function import target_function
 
 # Tworzymy graf
 G = generate_graph.generate_graph(
     number_of_nodes=10,
-    # max_base_distance=10.0,
+    max_base_distance=50.0,
     # reward_range=(5, 20),
     # cost_factor=1.0
 )
 
-draw_graph.draw_graph(G, "Example 3D Graph")
+path = ['P', 's1', 's2', 's3', 'P']
+print(target_function(G, path))
+
+
+draw_graph.draw_graph(G, "Example 3D Graph", path)
---
+++ target_function.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+def target_function(graph: nx.Graph, path: list[str]) -> float:
+    rewards = sum(
+        data['reward']
+        for (node, data) in graph.nodes(data=True)
+        if node in path
+    )
+    costs = sum(
+        graph[u][v]['cost']
+        for u, v in zip(path[:-1], path[1:])
+    )
+    return rewards / costs
