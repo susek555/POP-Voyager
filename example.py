3dc425f4946401e6a4ee71f36a6ea7731a96f18f
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-09 18:24:59 +0100

    change heuristics imports to explicit

--- example.py
+++ example.py
@@ -1,27 +1,28 @@
-import heuristics
 from graph.generate import generate_graph
+from heuristics.a_star import a_star
+from heuristics.ant_colony import aco
+from heuristics.genetic import genetic
+from heuristics.greedy import greedy
+from heuristics.random import full_random
 from objective_function import objective_function
 from utils.ant_colony import AcoParams
 from utils.genetic import GeneticParams, ordered_crossover, select_tournament
-from utils.sa import SAparams
 
 G = generate_graph(
-    number_of_nodes=20,
-    max_base_distance=50.0,
-    reward_range=(10, 40),
-    cost_factor=0.2,
+    number_of_nodes=500,
+    max_base_distance=100.0,
+    reward_range=(10, 100),
+    cost_factor=0.5,
 )
 
-path = heuristics.full_random(G, 6)
+path = full_random(G, 100)
 print(f"Random: {objective_function(G, path)}")
-path = heuristics.greedy(G, 6)
+path = greedy(G, 100)
 print(f"Greedy: {objective_function(G, path)}")
-path = heuristics.SA(G, objective_function, 6, SAparams(1000, 500, 0.995, 4, 4))
-print(f"SA: {objective_function(G, path)}")
-path = heuristics.genetic(
+path = genetic(
     G,
     objective_function,
-    6,
+    100,
     GeneticParams(
         pop_size=30,
         generations=100,
@@ -32,22 +33,23 @@ path = heuristics.genetic(
     ),
 )
 print(f"Genetic: {objective_function(G, path)}")
-path = heuristics.aco(
+path = aco(
     G,
     objective_function,
-    6,
+    100,
     AcoParams(
-        ant_count=20,
-        iteration_count=100,
-        alpha=0.8,
-        beta=1.5,
+        ant_count=80,
+        iteration_count=600,
+        alpha=1,
+        beta=2,
         pheromone_degradation_rate=0.1,
-        Q=100,
+        Q=300,
+        candidate_list_size=60,
     ),
 )
 print(f"ACO: {objective_function(G, path)}")
 
-path = heuristics.a_star(G, 6)
+path = a_star(G, 100)
 print(f"A_star: {objective_function(G, path)}")
 
 
--- heuristics/__init__.py
+++
@@ -1,6 +0,0 @@
-from .a_star import a_star
-from .ant_colony import aco as aco
-from .genetic import genetic as genetic
-from .greedy import greedy as greedy
-from .random import full_random as full_random
-from .sa import SA as SA
