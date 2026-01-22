56be9abb68785c6b86426900be7df2ef91d07562
Michal <01187288@pw.edu.pl>
2026-01-22 12:34:35 +0100

    Refactored scenarios experiments

--- experiment/experiments/graph_scenarios/compare_on_nebula_scenario.py
+++ experiment/experiments/graph_scenarios/compare_on_nebula_scenario.py
@@ -1,14 +1,11 @@
+from experiment.experiments.graph_scenarios.get_compare_on_scenarios_experiment import (
+    get_compare_on_scenarios_experiment,
+)
 from experiment.runner import Experiment, ExperimentRunner
 from graph.config import Graph, GraphScenario
-from graph.scenarios.nebula import NebulaGraphParams  # Zakładana ścieżka importu
-from utils.a_star import AStarParams, ChildrenFactory
-from utils.ant_colony.common import AcoParams
-from utils.config import Algorithm, AlgorithmParams, AlgorithmType
-from utils.genetic import GeneticParams, ordered_crossover, select_tournament
-from utils.sa import SAparams
+from graph.scenarios.nebula import NebulaGraphParams
 
 PATH_NODES = 10
-TIMES_TO_RUN = 30
 EXP_NAME = "compare_on_nebula_scenario"
 
 NEBULA_SETUP = Graph(
@@ -20,138 +17,18 @@ NEBULA_SETUP = Graph(
         nebula_radius=20.0,
         nebula_multiplier=4.0,
         reward_range=(15, 60),
-        cost_factor=0.2
+        cost_factor=0.2,
     ),
 )
 
-# SA
-DEFAULT_N_ITER = 5000
-DEFAULT_START_TEMP = 10
-DEFAULT_DECREASE_FACTOR = 0.995
-
-# GENETIC
-DEFAULT_POP_SIZE = 500
-DEFAULT_GENERATIONS = 500
-DEFAULT_MUTATION_RATE = 0.1
-DEFAULT_TOURNAMENT_SIZE = 2
-
-# ACO
-DEFAULT_ANT_COUNT = 50
-DEFAULT_ITERATIONS = 800
-DEFAULT_ALPHA = 1.0
-DEFAULT_BETA = 1.0
-DEFAULT_DEGRADATION = 0.3
-DEFAULT_Q = 50
-DEFAULT_CANDIDATES_LIST_SIZE = 10
-
-# A_STAR
-DEFAULT_N_CHILDREN = 10
-
-def random_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(type=AlgorithmType.RANDOM, params=AlgorithmParams()),
-        nodes=PATH_NODES,
-        times_to_run=TIMES_TO_RUN,
-    )
-
-def greedy_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(type=AlgorithmType.GREEDY, params=AlgorithmParams()),
-        nodes=PATH_NODES,
-        times_to_run=TIMES_TO_RUN,
-    )
-
-def sa_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(
-            type=AlgorithmType.SA,
-            params=SAparams(
-                n_iter=DEFAULT_N_ITER,
-                start_temp=DEFAULT_START_TEMP,
-                decrease_factor=DEFAULT_DECREASE_FACTOR,
-                n_threads=4,
-                n_candidates_per_thread=4,
-            ),
-        ),
-        nodes=PATH_NODES,
-        times_to_run=TIMES_TO_RUN,
-    )
-
-def ga_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(
-            type=AlgorithmType.GENETIC,
-            params=GeneticParams(
-                pop_size=DEFAULT_POP_SIZE,
-                generations=DEFAULT_GENERATIONS,
-                mutation_rate=DEFAULT_MUTATION_RATE,
-                crossover=ordered_crossover,
-                selection=select_tournament,
-                selection_kwargs={"tournament_size": DEFAULT_TOURNAMENT_SIZE},
-            ),
-        ),
-        nodes=PATH_NODES,
-        times_to_run=TIMES_TO_RUN,
-    )
-
-def aco_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(
-            type=AlgorithmType.ACO,
-            params=AcoParams(
-                ant_count=DEFAULT_ANT_COUNT,
-                iteration_count=DEFAULT_ITERATIONS,
-                alpha=DEFAULT_ALPHA,
-                beta=DEFAULT_BETA,
-                pheromone_degradation_rate=DEFAULT_DEGRADATION,
-                Q=DEFAULT_Q,
-                candidate_list_size=DEFAULT_CANDIDATES_LIST_SIZE
-            ),
-        ),
-        nodes=PATH_NODES,
-        times_to_run=TIMES_TO_RUN,
-    )
-
-def a_star_compare_on_nebula_scenario() -> Experiment:
-    return Experiment(
-        name=EXP_NAME,
-        graph=NEBULA_SETUP,
-        algorithm=Algorithm(
-            type=AlgorithmType.A_STAR,
-            params=AStarParams(
-                childrenFactory=ChildrenFactory.N_BEST,
-                n_children=DEFAULT_N_CHILDREN
-            ),
-        ),
-        nodes=PATH_NODES,
-        times_to_run=1,
-    )
 
 def get_all_compare_on_nebula_scenario() -> list[Experiment]:
-    experiments = []
-    experiments.append(random_compare_on_nebula_scenario())
-    experiments.append(greedy_compare_on_nebula_scenario())
-    experiments.append(sa_compare_on_nebula_scenario())
-    experiments.append(ga_compare_on_nebula_scenario())
-    experiments.append(aco_compare_on_nebula_scenario())
-    experiments.append(a_star_compare_on_nebula_scenario())
-    return experiments
+    return get_compare_on_scenarios_experiment(PATH_NODES, EXP_NAME, NEBULA_SETUP)
+
 
 if __name__ == "__main__":
-    all_experiments = get_all_compare_on_nebula_scenario()
+    all_compare_experiments = get_all_compare_on_nebula_scenario()
 
     ExperimentRunner.run_parallel(
-        experiments=all_experiments,
-        max_workers=4,
-        reuse_graph=True
+        experiments=all_compare_experiments, max_workers=4, reuse_graph=True
     )
---
+++ experiment/experiments/graph_scenarios/get_compare_on_scenarios_experiment.py
@@ -0,0 +1,150 @@
+from experiment.runner import Experiment
+from graph.config import Graph
+from utils.a_star import AStarParams, ChildrenFactory
+from utils.ant_colony.common import AcoParams
+from utils.config import Algorithm, AlgorithmParams, AlgorithmType
+from utils.genetic import GeneticParams, ordered_crossover, select_tournament
+from utils.sa import SAparams
+
+TIMES_TO_RUN = 30
+
+# SA
+DEFAULT_N_ITER = 5000
+DEFAULT_START_TEMP = 10
+DEFAULT_DECREASE_FACTOR = 0.995
+
+# GENETIC
+DEFAULT_POP_SIZE = 500
+DEFAULT_GENERATIONS = 500
+DEFAULT_MUTATION_RATE = 0.1
+DEFAULT_TOURNAMENT_SIZE = 2
+
+# ACO
+DEFAULT_ANT_COUNT = 50
+DEFAULT_ITERATIONS = 800
+DEFAULT_ALPHA = 1.0
+DEFAULT_BETA = 1.0
+DEFAULT_DEGRADATION = 0.3
+DEFAULT_Q = 50
+DEFAULT_CANDIDATES_LIST_SIZE = 10
+
+# A_STAR
+DEFAULT_N_CHILDREN = 10
+
+
+def get_compare_on_scenarios_experiment(
+    PATH_NODES: int, EXP_NAME: str, GraphSetup: Graph
+) -> list[Experiment]:
+    experiments = []
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.RANDOM,
+                params=AlgorithmParams(),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.GREEDY,
+                params=AlgorithmParams(),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.SA,
+                params=SAparams(
+                    n_iter=DEFAULT_N_ITER,
+                    start_temp=DEFAULT_START_TEMP,
+                    decrease_factor=DEFAULT_DECREASE_FACTOR,
+                    n_threads=4,
+                    n_candidates_per_thread=4,
+                ),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.RANDOM,
+                params=AlgorithmParams(),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.GENETIC,
+                params=GeneticParams(
+                    pop_size=DEFAULT_POP_SIZE,
+                    generations=DEFAULT_GENERATIONS,
+                    mutation_rate=DEFAULT_MUTATION_RATE,
+                    crossover=ordered_crossover,
+                    selection=select_tournament,
+                    selection_kwargs={"tournament_size": DEFAULT_TOURNAMENT_SIZE},
+                ),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.ACO,
+                params=AcoParams(
+                    ant_count=DEFAULT_ANT_COUNT,
+                    iteration_count=DEFAULT_ITERATIONS,
+                    alpha=DEFAULT_ALPHA,
+                    beta=DEFAULT_BETA,
+                    pheromone_degradation_rate=DEFAULT_DEGRADATION,
+                    Q=DEFAULT_Q,
+                    candidate_list_size=DEFAULT_CANDIDATES_LIST_SIZE,
+                ),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=TIMES_TO_RUN,
+        )
+    )
+
+    experiments.append(
+        Experiment(
+            name=EXP_NAME,
+            graph=GraphSetup,
+            algorithm=Algorithm(
+                type=AlgorithmType.A_STAR,
+                params=AStarParams(
+                    childrenFactory=ChildrenFactory.N_BEST, n_children=DEFAULT_N_CHILDREN
+                ),
+            ),
+            nodes=PATH_NODES,
+            times_to_run=1,
+        )
+    )
+
+    return experiments
